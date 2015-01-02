0bc92afdd445292b5eb687d58d8742c3b9227715
Eric Kennedy <ekeric13@gmail.com>
2015-01-02 11:00:58 -0800

    follow video

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -2,6 +2,19 @@
 var SongQueue = Songs.extend({
 
   initialize: function(){
+    this.on("add", this.enqueue, this);
+    this.on("dequeue", this.dequeue, this);
+    this.on( "ended", this.playNext, this);
+  },
+
+  enqueue : function(song){
+    if ( this.length === 1){
+      this.playFirst();
+    }
+  },
+
+  playFirst: function(){
+    this.at(0).play()
   }
 
 });
